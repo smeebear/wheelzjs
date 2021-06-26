104b4adff1d2da3a330408c13657b87578cbea4d
SmeeBear <soheron@gmail.com>
2021-06-26 09:30:48 -0500

    Added wheelz

---
+++ index.js
@@ -0,0 +1,17 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const dotenv = require('dotenv');
+
+client.once('ready', () => {
+    console.log("Ready!");
+})
+
+client.login(process.env.TOKEN);
+
+const wheelz = client.emojis.cache.find(emoji => emoji.name === "wheelz")
+
+client.on('message', message => {
+    if (message.content === '!wheelz') {
+        message.channel.send(`THE WHEELZZZZ ${wheelz}`)
+    }
+})
