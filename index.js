c8151d57a256e73cbb0b4659b48b76f4bf63fc1a
SmeeBear <soheron@gmail.com>
2021-06-28 08:07:49 -0500

    More functionality

--- index.js
+++ index.js
@@ -8,12 +8,25 @@ client.once('ready', () => {
 
 client.login(process.env.TOKEN);
 
-// const wheelz = client.emojis.cache.find(emoji => emoji.name === "wheelz")
-
 client.on('message', message => {
-    if (message.content.match(/wheelz/gi) && !(message.author.bot)) {
+    if (message.author.bot) return;
+    if (message.content.match(/wheelz/gi)) {
         console.log(message.content);
         const wheelz = client.emojis.cache.find(emoji => emoji.name === "wheelz");
         message.channel.send(`THE WHEELZZZZ ${wheelz}`);
+    }
+    else if (message.content.match(/bones/gi)) {
+        console.log(message.content);
+        const elmo = client.emojis.cache.find(emoji => emoji.name === "flamingelmo");
+        message.channel.send(`BOOOOOOOOOONES ${elmo}`)
+    }
+    
+    if (message.content === "!roll") {
+        const rolled = Math.ceil(Math.random() * 20)
+        message.channel.send(`You rolled a ${rolled}!`)
+    }
+
+    if (message.content === "!hug") {
+        message.channel.send(`Have a hug, ${message.member.user}! (づ￣ ³￣)づ `)
     }
 })
